7ef3a967311e571c7f8f6956e3c51358ef5db918
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-16 14:14:49 -0700

    Temp dir is consistent on macos and windows

--- redwyoming.go
+++ redwyoming.go
@@ -1,33 +1,17 @@
 package redwyoming
 
 import (
-	"fmt"
 	"log/slog"
 	"os"
-	"runtime"
 )
 
 func Main() {
 	slog.Debug("redwyoming", "test", true)
 
 	test1()
-	test2()
 }
 
 func test1() {
 	tmpDir := os.TempDir()
-	fmt.Printf("Temporary directory: %s\n", tmpDir)
-}
-
-func getTemporaryDirectory() string {
-	if runtime.GOOS == "windows" {
-		return os.TempDir()
-	} else {
-		return "/tmp"
-	}
-}
-
-func test2() {
-	tmpDir := getTemporaryDirectory()
-	fmt.Printf("Temporary directory: %s\n", tmpDir)
+	slog.Debug("temporary directory", "directory", tmpDir)
 }
